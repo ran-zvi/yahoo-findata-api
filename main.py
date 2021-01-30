b833be60efc32b9a1ccb11c59861c0eac950e0fd
rantzvi <33847526+rantzvi@users.noreply.github.com>
2021-01-31 01:18:32 +0200

    add procfile.txt

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/data/{symbol}')
+@app.get('/')
+def read_root():
+    return {'message': 'yahoo-findata-api'}
+
+
+@app.get('/{symbol}')
 def get_fin_data(symbol: str, type: str):
     f_type = FINANCIAL_DATA_TYPES_MAP.get(type, type)
     if f_type not in FINANCIAL_DATA_TYPES_MAP.values():
